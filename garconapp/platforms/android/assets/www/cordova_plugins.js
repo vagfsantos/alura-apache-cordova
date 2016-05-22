57e7f2d7563c2b53812f7dbd629ebbd7d1020e8c
Vagner Santos <santos.vagnerferreira@gmail.com>
2016-05-22 01:12:43 -0300

    final prototype

--- garconapp/platforms/android/assets/www/cordova_plugins.js
+++ garconapp/platforms/android/assets/www/cordova_plugins.js
@@ -1,9 +1,27 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.2"
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-statusbar": "2.1.3",
+    "cordova-plugin-compat": "1.0.0",
+    "phonegap-plugin-barcodescanner": "5.0.0"
 };
 // BOTTOM OF METADATA
 });
